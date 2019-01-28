f866026a93418969c0a72cb70b5073b559ae0822
Tamas Palinkas <tamaspalinkas1@gmail.com>
2019-01-28 13:25:39 +0100

    Add hydra-api script

---
+++ week-03/hydra-api.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import requests
+import json
+
+ids = pd.read_csv('~/greenfox/data-science/week-03/top_movie_ids_1k.csv')
+imdb_ids = ids['tconst'].tolist()
+
+url = 'https://hydramovies.com/api-v2/?source=http://hydramovies.com/api-v2/current-Movie-Data.csv&imdb_id='
+req_cnt = 0
+mv_data = []
+testlist = imdb_ids[:5]
+
+for imdb_id in imdb_ids:
+    content = requests.get(url + imdb_id).json()
+    if not content:
+        print(imdb_id + ' failed')
+    try:
+        for key in content:
+            for item in content.items():    
+                mv_data.append([content[key]['imdb_id'], content[key]['Title'],
+                content[key]['Categories'], content[key]['runtime'], content[key]['ytid'], content[key]['summary']])
+                print(str(imdb_id) + ' completed')
+    except (IndexError, KeyError, TypeError) as error:
+        print(str(imdb_id) + ' failed with: ' + str(error))
+
+mv_df = pd.DataFrame(mv_data)
+print(mv_df.head())
+mv_df.to_csv('hydra_test.csv')
