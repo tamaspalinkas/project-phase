f0a97e494d839eb1bea4e0356c34b08107235dcc
Tamas Palinkas <tamaspalinkas1@gmail.com>
2019-01-30 13:30:36 +0100

    Update baic script

--- week-03/hydra_basic.py
+++ week-03/hydra_basic.py
@@ -9,22 +9,24 @@ imdb_ids = ids['tconst'].tolist()
 url = 'https://hydramovies.com/api-v2/?source=http://hydramovies.com/api-v2/current-Movie-Data.csv&imdb_id='
 req_cnt = 0
 mv_data = []
-testlist = imdb_ids[:5]
+names = ['imdb_id', 'Title', 'Categories', 'runtime', 'ytid', 'summary']
 
-for imdb_id in testlist:
+for imdb_id in imdb_ids:
     content = requests.get(url + imdb_id).json()
     if not content:
         print(imdb_id + ' failed')
     try:
         for key in content:
-            for item in content.items():    
-                mv_data.append([content[key]['imdb_id'], content[key]['Title'],
-                content[key]['Categories'], content[key]['runtime'], content[key]['ytid'], content[key]['summary']])
-                print(str(imdb_id) + ' completed')
+            result = {}
+            for name in names:
+                if name in content[key]:
+                    result[name] = content[key][name]
+                else:
+                    result[name] = None
+            mv_data.append([result[name] for name in names])
+            print(str(imdb_id) + ' completed')
     except (IndexError, KeyError, TypeError) as error:
         print(str(imdb_id) + ' failed with: ' + str(error))
 
-print(mv_data)
 mv_df = pd.DataFrame(mv_data)
-print(mv_df.head())
-mv_df.to_csv('~/greenfox/project-phase/week-03/mp_test.csv', index=False)
+mv_df.to_csv('~/greenfox/project-phase/week-03/hydra_1k_basic.csv', index=False)
